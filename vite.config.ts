3b7e22ea7b30cc6abe62949f1612bf7100dee9ef
Yannick Debrée <yannickdebree@laposte.net>
2023-04-28 12:10:14 +0200

    Skeleton created

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import { dirname, join } from 'path'
+
+function getFolderAbsolutePath(pathFromProjectRootPath: string) {
+    const projectRootPath = dirname(__filename);
+    return join(projectRootPath, pathFromProjectRootPath);
+}
+
+export default defineConfig({
+    root: getFolderAbsolutePath('src'),
+    publicDir: getFolderAbsolutePath('public'),
+    server: {
+        port: 3000
+    },
+    build: {
+        outDir: getFolderAbsolutePath('docs'),
+        emptyOutDir: true,
+        copyPublicDir: true
+    }
+});
